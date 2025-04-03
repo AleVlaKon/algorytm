349587b140faaaa45f9e5cf05772536ed45457a4
Алекс <kontikov@mail.ru>
2025-04-03 13:08:21 +0500

    Решение 2.2.18 вариант 2 (без eval)

--- 2/2.2.18.py
+++ 2/2.2.18.py
@@ -1,10 +1,17 @@
 def calculate(vars: str, values: list[int], exp: str) -> int:
-    combination_values = zip(vars, values)
-    dict_of_values = {k: v for k, v in combination_values}
-    result = eval(exp, dict_of_values)
+    dict_of_values = dict(zip(vars, values))
+    result = 0
+    coef = 1
+    for char in exp:
+        if char == '-':
+            coef = -1
+        elif char == '+':
+            coef = 1
+        elif char in dict_of_values:
+            result += dict_of_values[char]*coef
     return result
 
 
 
 if __name__ == "__main__":
-    calculate('ab', [2, 2], 'a+b')
+    print(calculate('ab', [2, 2], 'a+b'))
