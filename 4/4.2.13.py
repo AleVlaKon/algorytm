a617e130470c71c6355b1d81acf86bfe148877b0
AleVlaKon <kontikov@mail.ru>
2025-05-11 00:06:25 +0300

    Сделал задачу 4.2.13 через итератор (accumulate)

--- 4/4.2.13.py
+++ 4/4.2.13.py
@@ -1,3 +1,6 @@
+from itertools import accumulate
+
+
 def avg_values(nums: list):
     list_of_avg_values = []
     sum_i_values = 0
@@ -5,6 +8,16 @@ def avg_values(nums: list):
         sum_i_values += nums[i]
         list_of_avg_values.append(sum_i_values / (i + 1))
     return list_of_avg_values
+
+
+def avg_values(nums: list):
+    list_of_avg_values = []
+    for i, sum in enumerate(accumulate(nums), 1):
+        list_of_avg_values.append(sum / i)
+    return list_of_avg_values
+
+
+# print(list(enumerate(accumulate([10, 20, 30, 40, 50]), 1)))
 
 
 print(avg_values([10, 20, 30, 40, 50]))
--- tests/testing_func.py
+++ tests/testing_func.py
@@ -1,7 +1,16 @@
+from itertools import accumulate
+
 def avg_values(nums: list):
     list_of_avg_values = []
     sum_i_values = 0
     for i in range(len(nums)):
         sum_i_values += nums[i]
         list_of_avg_values.append(sum_i_values / (i + 1))
+    return list_of_avg_values
+
+
+def avg_values(nums: list):
+    list_of_avg_values = []
+    for sum, i in enumerate(accumulate(nums), 1):
+        list_of_avg_values.append(sum / i)
     return list_of_avg_values
