7249162b0a59fb04de56d04b3f100cb6082811e2
Алекс <kontikov@mail.ru>
2025-05-12 18:46:42 +0300

    Сделана задача 4.2.15 (свое решение)

---
+++ 4/4.2.15.py
@@ -0,0 +1,30 @@
+import math
+
+
+def rotate_number(number: int) -> int:
+    new_digit = 0
+    log_n = math.log10(number)
+    len_number = math.floor(log_n) + 1
+    for i in range(len_number - 1, -1, -1):
+        last_digit = number % 10
+        new_digit += last_digit * 10 ** i
+        number //= 10
+    return new_digit
+
+
+def make_palindrome(n: int):
+    if n == rotate_number(n):
+        return n
+    else:
+        new_n = n
+        for i in range(5):
+            rotated_num = rotate_number(new_n)
+            new_n += rotated_num
+            if new_n == rotate_number(new_n):
+                return new_n
+
+
+    return -1
+
+
+print(make_palindrome(166))
