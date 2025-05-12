4615920214a0cf3ebeba7ec05b8bcf0014b0c1bf
Алекс <kontikov@mail.ru>
2025-05-12 18:56:41 +0300

    Сделана задача 4.2.15 (решение поколения)

--- tests/testing_func.py
+++ tests/testing_func.py
@@ -1,27 +1,21 @@
-import math
-
-
 def rotate_number(number: int) -> int:
     new_digit = 0
-    log_n = math.log10(number)
-    len_number = math.floor(log_n) + 1
-    for i in range(len_number - 1, -1, -1):
+    while number > 0:
         last_digit = number % 10
-        new_digit += last_digit * 10 ** i
+        new_digit = new_digit * 10 + last_digit
         number //= 10
     return new_digit
 
 
 def make_palindrome(n: int):
-    if n == rotate_number(n):
-        return n
-    else:
-        new_n = n
-        for i in range(5):
-            rotated_num = rotate_number(new_n)
-            new_n += rotated_num
-            if new_n == rotate_number(new_n):
-                return new_n
+    for i in range(6):
+        new_n = rotate_number(n)
+        if new_n == n:
+            return new_n
+        else:
+            n += new_n
 
 
     return -1
+
+
