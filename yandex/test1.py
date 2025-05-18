c3240496399f924ddc9d774e100ed3a653e2dfc1
AleVlaKon <kontikov@mail.ru>
2025-05-18 22:19:16 +0300

    переработал задачу про 3 единицы под требования сдачи (и сдал)

--- yandex/test1.py
+++ yandex/test1.py
@@ -1,11 +1,12 @@
-def three_points_in_row(n: int):
-    dp = [0] * (n + 4)
+# def three_points_in_row(n: int):
+#     n = int(input())
+#     dp = [0] * (n + 4)
 
-    dp[1], dp[2], dp[3] = 2, 4, 7
-    for i in range(4, n + 1):
-        dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 12345
+#     dp[1], dp[2], dp[3] = 2, 4, 7
+#     for i in range(4, n + 1):
+#         dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 12345
 
-    return dp[n]
+#     return dp[n]
 
 def three_points_in_row(n: int):
     dp = [0, 2, 4, 7]
@@ -13,8 +14,8 @@ def three_points_in_row(n: int):
     for i in range(4, n + 1):
         next_digit = dp[i - 3] + dp[i - 2] + dp[i - 1]
         dp[i] = next_digit
-    return dp
+    return dp[n]
 
 
 
-print(three_points_in_row(8))
+print(three_points_in_row(int(input())))
