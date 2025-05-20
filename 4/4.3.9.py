8d645891d5aff791d8e4e60cca3df2dff760bacd
AleVlaKon <ex@example.com>
2025-05-20 14:57:15 +0300

    Сделана задача 4.3.9

--- 4/4.3.9.py
+++ 4/4.3.9.py
@@ -4,3 +4,12 @@ def wave(nums: list[int]) -> None:
             nums[i], nums[i-1] = nums[i-1], nums[i]
         elif i % 2 == 1 and nums[i-1] < nums[i]:
             nums[i], nums[i-1] = nums[i-1], nums[i]
+
+
+data = [1, 2, 3, 4, 5]
+wave(data)
+print(data)
+
+data = [2, 4, 7, 8, 9, 10]
+wave(data)
+print(data)
