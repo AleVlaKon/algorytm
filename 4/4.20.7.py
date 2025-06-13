9a69d60d63a86f48333c91eacce9e4fb8424b249
AleVlaKon <kontikov@mail.ru>
2025-06-13 23:28:21 +0200

    Немного улучшил задача 4.20.7

--- 4/4.20.7.py
+++ 4/4.20.7.py
@@ -1,25 +1,17 @@
 def diff_even_odd(a: int, b: int) -> int:
-    if a % 2 == 0:
-        first_chet = a
-        first_nechet = a + 1
-    elif a % 2 == 1:
-        first_chet = a + 1
-        first_nechet = a
+    first_chet = a + a % 2
+    first_nechet = a - a % 2 + 1
 
-    if b % 2 == 0:
-        last_chet = b
-        last_nechet = b - 1 
-    elif b % 2 == 1:
-        last_chet = b - 1
-        last_nechet = b
+    last_chet = b - b % 2
+    last_nechet = b + b % 2 - 1 
     
-    n_chet = (last_chet - first_chet) / 2 + 1
-    n_nechet = (last_nechet - first_nechet) / 2 + 1
+    n_chet = (last_chet - first_chet) // 2 + 1
+    n_nechet = (last_nechet - first_nechet) // 2 + 1
 
-    summ_chet = (first_chet + last_chet) * n_chet / 2
-    summ_nechet = (first_nechet + last_nechet) * n_nechet / 2
+    summ_chet = (first_chet + last_chet) * n_chet // 2
+    summ_nechet = (first_nechet + last_nechet) * n_nechet // 2
 
-    return int(summ_chet - summ_nechet)
+    return summ_chet - summ_nechet
 
 
 
