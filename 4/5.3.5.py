0441444d4ebf7359e552496c0348d705d0a06b63
AleVlaKon <kontikov@mail.ru>
2025-06-21 00:25:20 +0300

    Решена задача 5.3.5

--- 4/5.3.5.py
+++ 4/5.3.5.py
@@ -7,3 +7,10 @@ def fizz_buzz(n: int) -> tuple:
     to_three = floor(new_n / 3) - to_fifteen
     to_five = floor(new_n / 5) - to_fifteen
     return to_three, to_five, to_fifteen
+
+
+
+print(fizz_buzz(20))
+
+print(fizz_buzz(5))
+print(fizz_buzz(31))
