edd02b52e2e413681b95eefa4c5367b4b1dfa458
Алекс <kontikov@mail.ru>
2025-07-01 23:26:40 +0300

    Сделал задачу 6.1.12

--- 6/6.1.12.py
+++ 6/6.1.12.py
@@ -3,3 +3,9 @@ def equal(nums: list[int]):
         if nums[i] == i:
             return i
     return -1
+
+
+print(equal([10, 7, 2]))
+print(equal([2, 1, 4, 3]))
+print(equal([2, 9, 4, 8]))
+print(equal([0, 1, 2, 3]))
