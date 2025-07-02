e97bf9219d779bd81c32864a70e30713a5209571
Алекс <kontikov@mail.ru>
2025-07-02 22:35:16 +0300

    Сделал задачу 6.1.16

--- 6/6.1.16.py
+++ 6/6.1.16.py
@@ -6,3 +6,9 @@ def find_number(nums):
     for i in range(1, len(nums)):
         if  nums[i-1] < nums[i] < nums[i + 1]:
             return nums[i]
+
+
+print(find_number([1, 1, 2, 3, 3]))
+print(find_number([1, 1, 5, 5, 40, 60, 60]))
+print(find_number([1, 1, 2, 2, 3, 3, 4]))
+print(find_number([2, 10, 10]))
