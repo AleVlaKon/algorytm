676b9c394d6c375fd146b3a4cca8eac2820c683c
Алекс <kontikov@mail.ru>
2025-07-03 22:30:26 +0300

    Сделал задачу 6.1.14

--- 6/6.1.14.py
+++ 6/6.1.14.py
@@ -12,3 +12,9 @@ def sum_digit(n):
         sum_digit += n % 10
         n //= 10
     return sum_digit
+
+print(count_numbers(13, 2))    # 10, 11, 12, 13
+print(count_numbers(10, 5))    # 10
+print(count_numbers(1, 0))     # 1
+print(count_numbers(10, 0))     # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
