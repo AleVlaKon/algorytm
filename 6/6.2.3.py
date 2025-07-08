6f2942fcba0062281adc7972d09bfaea4e956bf0
AleVlaKon <kontikov@mail.ru>
2025-07-08 23:11:16 +0300

    Решена задача 6.2.3

--- 6/6.2.3.py
+++ 6/6.2.3.py
@@ -6,3 +6,8 @@ def kth_occurrence(nums: list[int], target: int, k: int):
         if k_target == k:
             return i
     return -1
+
+
+print(kth_occurrence([1, 2, 3, 1, 2, 3, 1, 2, 3, 1], 3, 3))
+print(kth_occurrence([2, 2, 2, 2, 2], 2, 6))   
+print(kth_occurrence([5], 5, 1))               
