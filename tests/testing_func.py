d933e41bfab7bb3a44d65b198df87ea6a0d13cd9
AleVlaKon <kontikov@mail.ru>
2025-07-12 21:59:51 +0300

    Сделал задачу 6.2.7 (решение из комментариев 2-й вар)

--- tests/testing_func.py
+++ tests/testing_func.py
@@ -1,19 +1,10 @@
-def add_one(digits: list[int]) -> list[int]:
-    if digits[-1] != 9:
-        digits[-1] += 1
-        return digits
-    else:
-        digits[-1] += 1
-
-
-    for i in range(len(digits) -1, 0, -1):
-        if digits[i] == 10:
+def add_one(digits: list[int]):
+    for i in range(len(digits) - 1, -1, -1):
+        if digits[i] == 9:
             digits[i] = 0
-            digits[i - 1] += 1
+        else:
+            digits[i] += 1
+            return digits
 
-    if digits[0] == 10:
-        digits[:1] = [1, 0]
-
-    return digits
-
+    return [1] + digits
 
