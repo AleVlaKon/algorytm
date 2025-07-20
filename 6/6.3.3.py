2a1c1fa8380bb4b0b4ce68a334d93df20a8511fd
AleVlaKon <kontikov@mail.ru>
2025-07-20 23:21:01 +0300

    Сделал задачу 6.3.3 с использованием функции max_of_two

--- 6/6.3.3.py
+++ 6/6.3.3.py
@@ -1,14 +1,11 @@
+def max_of_two(a, b):
+    if a < b:
+        return b
+    return a
+
+
 def max_of_four(a, b, c, d):
-    result = float('-inf')
-    if a > result:
-        result = a
-    if b > result:
-        result = b
-    if c > result:
-        result = c
-    if d > result:
-        result = d
-    return result
+    return max_of_two(max_of_two(a, b), max_of_two(b, c))
 
 print(max_of_four(1, 2, 3, 1))
 print(max_of_four(1, 1, 1, 1))
