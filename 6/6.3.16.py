9b0b148b18ef71b216aa0d1b62ad55b4d0a918a6
AleVlaKon <ex@example.com>
2025-07-25 12:12:23 +0300

    Сделал 6.3.16 (правда с помощью DeepSeek

--- 6/6.3.16.py
+++ 6/6.3.16.py
@@ -1,12 +1,12 @@
-from itertools import pairwise
-
 def max_sum(nums):
-    pairs = pairwise(nums)
-    result = sum(next(pairs))
-    for pair in pairs:
-        result = max(result, sum(pair))
+    max_num  = float('-inf')
+    max_sum = 0
+    for i in nums:
+        if i > 0:
+            max_sum += i
+        if i > max_num:
+            max_num = i
+        
+    return max_sum if max_sum > 0 else max_num
 
-    return result
-
-
-print(max_sum([1, 2, -4, -2, 3]))   # 1 + 2 + 3
+print(max_sum([-1, 10, 5]))         # 10 + 5
