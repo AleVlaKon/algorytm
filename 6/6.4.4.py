76c9e1717275c1b7682b5c493c71982c202b4a0c
AleVlaKon <kontikov@mail.ru>
2025-07-28 23:21:01 +0300

    Решил задачу 6.4..4

--- 6/6.4.4.py
+++ 6/6.4.4.py
@@ -11,3 +11,8 @@ def get_closest_element(nums: list[int], target: int) -> int:
     return result
 
 
+print(get_closest_element([1, 2, 3, 4], 5))
+print(get_closest_element([1, -1, 3], 0))
+print(get_closest_element([1], 5))
+print(get_closest_element([5, 4, 3, 2, 1], 3))
+print(get_closest_element([1, 1, 1, 1, 1, 1], 2))
