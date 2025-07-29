59c64cce65207929ec0cc10a11610aecca52d118
AleVlaKon <kontikov@mail.ru>
2025-07-29 22:56:58 +0300

    Решил задачу 6.4.5 (за один проход)

--- tests/testing_func.py
+++ tests/testing_func.py
@@ -1,10 +1,15 @@
 def steps_to_max(nums: list[int]) -> int:
     max_element = nums[0]
+    count = 1
+    result = 0
     for i in range(1, len(nums)):
+        diff = abs(max_element - nums[i])
         if nums[i] > max_element:
             max_element = nums[i]
+            result += diff * count
+        else:
+            result += diff
+        count += 1
 
-    result = sum(max_element - i for i in nums)
     return result
-
 
