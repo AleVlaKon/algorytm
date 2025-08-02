540a49a2d6668e8cd08e7e7a7af0726661ca1651
Алекс <kontikov@mail.ru>
2025-08-02 21:59:38 +0300

    Сделал задачу 6.1.19

--- 6/6.1.19.py
+++ 6/6.1.19.py
@@ -3,3 +3,7 @@ def elements_in_the_range(nums: list[int], start: int, end: int):
     set_two = set(range(start, end + 1))
 
     return set_two.issubset(set_one)
+
+
+print(elements_in_the_range([1, 5, 6, 2, 3, 8, 9, 4], 1, 5))
+print(elements_in_the_range([1, 3, 5, 2, 7, 8, 4], 2, 6))
