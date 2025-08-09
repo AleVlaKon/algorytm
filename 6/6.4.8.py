40ce1cfa30a016febaedf222703777357468f271
AleVlaKon <kontikov@mail.ru>
2025-08-09 23:40:15 +0300

    Перерешил задачу 6.4.8

--- 6/6.4.8.py
+++ 6/6.4.8.py
@@ -1,22 +1,15 @@
 def max_sum(nums):
-    result = 0
-    current_result = 0
+    result = -1 
+    current_result = -1
     flag = False
-    check_num = False
-    for i in range(len(nums)):
-        if nums[i] < 0 and flag:
-            if current_result > result:
-                result = current_result
+    for num in nums:
+        if num < 0:
+            result = max(result, current_result)
             current_result = 0
-            check_num = True
-        elif nums[i] < 0:
             flag = True
-        elif nums[i] > 0 and flag:
-            current_result += nums[i]
-    if not check_num: 
-        return -1
+        elif flag:
+            current_result += num
     return result
-
 
 print(max_sum([1, -2, -3, 4]))
 print(max_sum([1, -2, 3, 4, -5]))
