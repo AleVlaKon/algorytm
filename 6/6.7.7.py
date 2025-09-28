25f844b1b42448581868475fa0dc629341da8013
AleVlaKon <kontikov@mail.ru>
2025-09-28 21:07:14 +0300

    Сделал задачу 6.7.7

--- 6/6.7.7.py
+++ 6/6.7.7.py
@@ -11,3 +11,11 @@ def search_insert_position(nums, target):
         elif elem < target:
             left = middle + 1
     return right + 1
+
+
+print(search_insert_position([1, 2, 3, 5, 6], 4))
+print(search_insert_position([1, 2, 3, 5, 6], 0))
+print(search_insert_position([1, 2, 3, 5, 6], 3))
+print(search_insert_position([1, 2, 3, 5, 6], 7))
+print(search_insert_position([0, 2, 4], 1))
+print(search_insert_position([-9, -8, -4, -3, 1, 2, 3, 7, 8, 9], 2))
