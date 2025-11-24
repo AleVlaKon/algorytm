73e7b0f7330ef139dab2ca76ccc83954a42f834e
AleVlaKon <kontikov@mail.ru>
2025-11-24 22:47:19 +0300

    Сделал задачу 8.1.19 (по методу из решений с operator)

--- 8/8.1.19.py
+++ 8/8.1.19.py
@@ -1,17 +1,15 @@
+from operator import lt, gt
+
 def alter_sort(nums):
     n = len(nums)
     for i in range(n - 1):
         key_index = i
-        if key_index % 2:
-            for j in range(i + 1, n):
-                if nums[j] < nums[key_index]:
-                    key_index = j
-            nums[i], nums[key_index] = nums[key_index], nums[i]
-        else:
-            for j in range(i + 1, n):
-                if nums[j] > nums[key_index]:
-                    key_index = j
-            nums[i], nums[key_index] = nums[key_index], nums[i]
+
+        condition = gt if key_index % 2 == 0 else lt
+        for j in range(i + 1, n):
+            if condition(nums[j], nums[key_index]):
+                key_index = j
+        nums[i], nums[key_index] = nums[key_index], nums[i]
 
 
 
