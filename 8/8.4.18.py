0c708f7a833e7f7c5afaf548508da9ceeefa830c
Алекс <kontikov@mail.ru>
2025-12-25 22:28:46 +0300

    Решил задачу 8.4.18 (с помощью словаря)

--- 8/8.4.18.py
+++ 8/8.4.18.py
@@ -1,13 +1,21 @@
-from collections import Counter
+def sort_limited_numbers(nums):
+    min_value, max_value = min(nums), max(nums)
+    counts = [0] * (max_value - min_value + 1)
 
-
-def sort_limited_numbers(data):
-    min_value, max_value = min(data), max(data)
-    counts = Counter(data)  # быстрое создание и заполнение вспомогательного словаря
+    for elem in nums:
+        counts[elem - min_value] += 1
 
     index = 0
 
-    for num in range(max_value, min_value - 1, -1):
-        for _ in range(counts.get(num, 0)):
-            data[index] = num
+    for num in range(max_value - min_value + 1):
+        for _ in range(counts[num]):
+            nums[index] = num + min_value
             index += 1
+
+nums = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+sort_limited_numbers(nums)
+print(nums)
+
+nums = [-15, 10, 1, -8, 24]
+sort_limited_numbers(nums)
+print(nums)
