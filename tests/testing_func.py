5c22f6d2c205944e65be6562b5517e330a241e5a
Алекс <kontikov@mail.ru>
2025-12-28 23:39:03 +0300

    Решил задачу 10.1.13

--- tests/testing_func.py
+++ tests/testing_func.py
@@ -1,13 +1,17 @@
 from itertools import accumulate
 
 
-def min_valid_index(nums, k, m):
+def difference_list(nums):
     total = list(accumulate(nums, initial=0))
+    result = []
 
-    for i in range(len(nums) - k):
-        if total[i + k + 1] - total[i] == m:
-            return i
+    for i in range(1, len(total)):
+        last = len(total) - 1
+        sum_right = total[last] - total[i]
+        sum_left = total[i - 1]
+        result.append(abs(sum_left - sum_right))
 
-    return -1
+
+    return result
 
 
