ab3c55af60d62d512b5a38f00479e6708f96f95e
AleVlaKon <kontikov@mail.ru>
2026-01-07 07:22:26 +0300

    Решил 10.2.2 через встречу посередине

--- 10/10.2.2.py
+++ 10/10.2.2.py
@@ -1,16 +1,19 @@
+from collections import defaultdict
+
 def happy_tickets(n):
-    count = 0
+    first_dict = defaultdict(int)
+    second_dict = defaultdict(int)
 
-    for i1 in range(10):
-        for i2 in range(10):
-            for i3 in range(10):
-                for i4 in range(10):
-                    if i1 + i2 + i3 + i4 == n:
-                        count += 1
-    return count ** 2
+    for a in range(10):
+        for b in range(10):
+            first_dict[a + b] += 1
 
-
-print(happy_tickets(1))
-print(happy_tickets(2))
-print(happy_tickets(3))
-print(happy_tickets(15))
+    for c in range(10):
+        for d in range(10):
+            second_dict[n - c - d] += 1
+    
+    counter = 0
+    for ket in first_dict:
+        counter += first_dict[ket] * second_dict[ket]
+    
+    return counter ** 2
