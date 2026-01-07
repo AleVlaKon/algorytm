88195530a8fdf08b472490e78c6cc1f3c99c4bbc
AleVlaKon <kontikov@mail.ru>
2026-01-07 07:53:16 +0300

    Решил задачу 10.2.4

--- 10/10.2.4.py
+++ 10/10.2.4.py
@@ -8,3 +8,7 @@ def pair_with_sum(nums: list[int], k: int):
     for i in nums:
         if i in result and i != k // 2:
             return (i, k - i) if i < k / 2 else (k - i, i)
+        
+
+print(pair_with_sum([1, 16, 9, 10, 4], 26))
+print(pair_with_sum([4, 5, -1, 10, -5], 0))
