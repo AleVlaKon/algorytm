aa4b810a48e9f3425475409d2ccd0c9dba075bc5
AleVlaKon <kontikov@mail.ru>
2026-01-09 23:18:15 +0300

    Решил 10.2.7, закрыл встречу посередине

--- 10/10.2.7.py
+++ 10/10.2.7.py
@@ -5,9 +5,8 @@ def count_valid_quadruplets(nums1, nums2, nums3, nums4):
     first_dict = defaultdict(int)
     second_dict = defaultdict(int)
 
-    func1 = lambda a, b: (a + b)
-    func2 = lambda c, d: (d - c)
-    func3 = lambda c, d: (3 * d - c)
+    func1 = lambda a, b: (a + b) ** 2
+    func2 = lambda c, d: (d - c) ** 2
 
 
     for a in nums1:
@@ -20,7 +19,6 @@ def count_valid_quadruplets(nums1, nums2, nums3, nums4):
                 second_dict[-c] += 1
             else:
                 second_dict[func2(c, d)] += 1
-                second_dict[func3(c, d)] += 1
 
     counter = 0
     for ket in first_dict:
@@ -29,10 +27,12 @@ def count_valid_quadruplets(nums1, nums2, nums3, nums4):
     return counter
 
 
-# print(count_valid_quadruplets([1, 2], [3, 4], [3, 6], [7, 8]))
-# print(count_valid_quadruplets([2, -1, 3], [-1, 0, 1], [4, 2, 5], [-2, 0]))
-# print(count_valid_quadruplets([1, 1, 3], [1, 1], [2, 7], [0, 0]))
-# print(count_valid_quadruplets([1], [2], [3], [4]))
-# print(count_valid_quadruplets([1, 1, 3], [1, 1], [2, 7], [0, 0]))
-# print(count_valid_quadruplets([0, 0], [0, 0], [0, 0], [0, 0]))
+print(count_valid_quadruplets([1, 2], [3, 4], [3, 6], [7, 8]))
+print(count_valid_quadruplets([2, -1, 3], [-1, 0, 1], [4, 2, 5], [-2, 0]))
+print(count_valid_quadruplets([1, 1, 3], [1, 1], [2, 7], [0, 0]))
+print(count_valid_quadruplets([1], [2], [3], [4]))
+print(count_valid_quadruplets([1, 1, 3], [1, 1], [2, 7], [0, 0]))
+print(count_valid_quadruplets([0, 0], [0, 0], [0, 0], [0, 0]))
+
+
 
