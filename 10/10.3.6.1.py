db390ec499ba905ae7f164b932b2287b61326a0b
AleVlaKon <kontikov@mail.ru>
2026-01-13 14:32:15 +0300

    Решил задачу 10.3.6.1

--- 10/10.3.6.1.py
+++ 10/10.3.6.1.py
@@ -24,3 +24,7 @@ def _merge_sorted_lists(nums1, nums2):
 def merge_sorted_lists(nums1, nums2, nums3):
     res = _merge_sorted_lists(nums1, nums2)
     return _merge_sorted_lists(res, nums3)
+
+
+print(merge_sorted_lists([1, 4, 6], [2, 4, 7], [2, 3, 5, 6]))
+print(merge_sorted_lists([-3, -2, -1], [4, 7], [0]))
