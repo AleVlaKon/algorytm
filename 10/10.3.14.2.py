b45b7c330f5869f0aad999adac40279a36626777
AleVlaKon <kontikov@mail.ru>
2026-02-04 20:49:14 +0300

    Решил задачу 10.3.14 сам за O(n)

--- 10/10.3.14.2.py
+++ 10/10.3.14.2.py
@@ -1,2 +1,31 @@
 def duplicate_zeros(nums: list[int]) -> None:
-    ...
+    res = []
+
+    left = 0
+    count = 0
+
+    while count < len(nums):
+        if nums[left] != 0:
+            res.append(nums[left])
+            left += 1
+            count += 1
+        else:
+            if nums[left] == 0:
+                res.append(0)
+                left += 1
+            if left < len(nums):
+                res.append(0)
+            count += 2
+                
+
+
+    for i in range(len(nums)):
+        nums[i] = res[i]
+    
+
+
+nums = [3, 0, 2, 7, 0, 1, 4, 5]
+duplicate_zeros(nums)
+print(nums)
+        
+    
--- tests/testing_func.py
+++ tests/testing_func.py
@@ -1,41 +1,23 @@
 def duplicate_zeros(nums: list[int]) -> None:
-    '''
-    1. Высчитываем индекс последнего элемента, который будет в списке
-    (длина списка - количество нулей)
-    2. Идем по списку справа налево от индекса последнего элемента
-    3. Если элемент == 0 - вставляем 2 нуля,
-       иначе вставляем элемент
-    '''
+    res = []
+
+    left = 0
     count = 0
 
-    for i in nums:
-        if i == 0:
+    while count < len(nums):
+        if nums[left] != 0:
+            res.append(nums[left])
+            left += 1
             count += 1
-
-    # if nums[-1] == 0:
-        # count -= 1
-
-    # if nums[-2] == 0:
-    #     count -= 1
-
-    count = len(nums) - count
-
-    read = count - 1
-    write = len(nums) - 1
-
-    while read >= 0:
-        nums_read = nums[read]
-        nums_write = nums[write]
-        if nums[read] == 0:
-            if write < len(nums):
-                nums[write] = 0
-                write -= 1
-            if write < len(nums):
-                nums[write] = 0
-                write -= 1
         else:
-            if write < len(nums):
-                nums[write] = nums[read]
-            write -= 1
+            if nums[left] == 0:
+                res.append(0)
+                left += 1
+            if left < len(nums):
+                res.append(0)
+            count += 2
+                
 
-        read -= 1
+
+    for i in range(len(nums)):
+        nums[i] = res[i]
