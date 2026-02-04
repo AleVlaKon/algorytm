f7ffdb196c871d6ea7eb55dac577047d86a33ab1
AleVlaKon <kontikov@mail.ru>
2026-02-04 20:55:23 +0300

    Коротенькое решение из комментариев
    Оно просто короткое, зато вспомнил, как присваивать значение списка другому списку

--- 10/10.3.14.2.py
+++ 10/10.3.14.2.py
@@ -2,26 +2,13 @@ def duplicate_zeros(nums: list[int]) -> None:
     res = []
 
     left = 0
-    count = 0
+    while left < len(nums):
+        if nums[left] == 0:
+            res.append(0)
+        res.append(nums[left])
+        left += 1
 
-    while count < len(nums):
-        if nums[left] != 0:
-            res.append(nums[left])
-            left += 1
-            count += 1
-        else:
-            if nums[left] == 0:
-                res.append(0)
-                left += 1
-            if left < len(nums):
-                res.append(0)
-            count += 2
-                
-
-
-    for i in range(len(nums)):
-        nums[i] = res[i]
-    
+    nums[:] = res[:len(nums)]
 
 
 nums = [3, 0, 2, 7, 0, 1, 4, 5]
