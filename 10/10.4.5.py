572ab73d2fbe714796c19a675c9a1ca6e028ee89
AleVlaKon <kontikov@mail.ru>
2026-02-11 16:05:57 +0300

    Задача 10.4.5 попытка 2 (пока не получилось)

--- 10/10.4.5.py
+++ 10/10.4.5.py
@@ -1,25 +1,27 @@
 def pair_with_lower_or_equal_difference(nums, k):
-    left = 0
-    right = 0
-    diff = float('inf')
-    curr_min_elem = nums[0]
-    curr_max_elem = nums[0]
-    min_elem = float('inf')
-    max_elem = 0
+    n = len(nums)
 
-    while right < len(nums) - 1:
-        if nums[right] - nums[left] <= k and right < len(nums) - 1:
-            right += 1
-            curr_max_elem = nums[right]
-        else:
-            while nums[right] - nums[left] > k:
-                left += 1
-                curr_min_elem = nums[left]
-            if max_elem - min_elem <= k and max_elem - min_elem > :
-                min_elem = curr_min_elem
-                max_elem = curr_max_elem
+    if n < 2:
+        return None
+    
+    left = 0
+    right = 1
+    curr_diff = 0
+    curr_min = nums[0]
+
+    while right < n:
+        diff = nums[right] - nums[left]
+        if diff <= k and diff > curr_diff:
+            min_elem = nums[left]
+            max_elem = nums[right]
+            curr_diff = diff
+            curr_min = min_elem
+        left += 1
+        right += 1
 
     return min_elem, max_elem
+            
 
 
-print(pair_with_lower_or_equal_difference([2, 4, 7, 8, 10, 11], 3))
+# print(pair_with_lower_or_equal_difference([2, 4, 7, 8, 10, 11], 3))
+print(pair_with_lower_or_equal_difference([2, 3, 4, 7, 8, 10, 11], 2))
