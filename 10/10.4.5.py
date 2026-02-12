4e3ba38efe6576d7427d3691ba8da05bc83077ec
AleVlaKon <kontikov@mail.ru>
2026-02-12 14:51:06 +0300

    Решение задачи 10.4.5 (из Perplexity)

--- 10/10.4.5.py
+++ 10/10.4.5.py
@@ -1,27 +1,33 @@
 def pair_with_lower_or_equal_difference(nums, k):
     n = len(nums)
-
     if n < 2:
         return None
-    
-    left = 0
-    right = 1
-    curr_diff = 0
-    curr_min = nums[0]
 
-    while right < n:
-        diff = nums[right] - nums[left]
-        if diff <= k and diff > curr_diff:
-            min_elem = nums[left]
-            max_elem = nums[right]
-            curr_diff = diff
-            curr_min = min_elem
-        left += 1
-        right += 1
+    l = 0
+    best_pair = None
+    best_diff = -1
 
-    return min_elem, max_elem
-            
+    for r in range(1, n):
+        # сдвигаем левый указатель, пока разность слишком большая
+        while l < r and nums[r] - nums[l] > k:
+            l += 1
+        if l == r:
+            continue
+
+        diff = nums[r] - nums[l]
+
+        if best_pair is None:
+            best_pair = (nums[l], nums[r])
+            best_diff = diff
+        else:
+            cur_min, cur_max = best_pair
+            if diff > best_diff:
+                best_diff = diff
+                best_pair = (nums[l], nums[r])
+            elif diff == best_diff and nums[l] < cur_min:
+                best_pair = (nums[l], nums[r])
+
+    return best_pair
 
 
-# print(pair_with_lower_or_equal_difference([2, 4, 7, 8, 10, 11], 3))
 print(pair_with_lower_or_equal_difference([2, 3, 4, 7, 8, 10, 11], 2))
--- tests/testing_func.py
+++ tests/testing_func.py
@@ -1,12 +1,32 @@
-def count_pairs_with_greater_difference(nums, k):
-    left, right, count = 0, 0, 0
+from typing import List, Optional, Tuple
+
+def pair_with_lower_or_equal_difference(nums, k):
     n = len(nums)
+    if n < 2:
+        return None
 
-    while right < n:
-        if nums[right] - nums[left] > k:
-            count += n - right
-            left += 1
+    l = 0
+    best_pair = None
+    best_diff = -1
+
+    for r in range(1, n):
+        # сдвигаем левый указатель, пока разность слишком большая
+        while l < r and nums[r] - nums[l] > k:
+            l += 1
+        if l == r:
+            continue
+
+        diff = nums[r] - nums[l]
+
+        if best_pair is None:
+            best_pair = (nums[l], nums[r])
+            best_diff = diff
         else:
-            right += 1
+            cur_min, cur_max = best_pair
+            if diff > best_diff:
+                best_diff = diff
+                best_pair = (nums[l], nums[r])
+            elif diff == best_diff and nums[l] < cur_min:
+                best_pair = (nums[l], nums[r])
 
-    return count
+    return best_pair
