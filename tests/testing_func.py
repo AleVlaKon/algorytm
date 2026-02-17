f27cac77bfbddc52a38d5545a5d9815bb9feddb9
AleVlaKon <kontikov@mail.ru>
2026-02-17 16:40:09 +0300

    Задача 10.5.3 в.2 (все еще не работает)

--- tests/testing_func.py
+++ tests/testing_func.py
@@ -1,13 +1,28 @@
 def negatives_count_in_every_sublist(nums, k):
-    left = 0
+    left = 1
     right = k
     res = []
+    count = 0
 
-    for i in range(right, len(nums) + 1):
-        # print(nums[i - k:i])
-        count = 0
-        for j in range(i - k, i):
-            if nums[j] < 0:
-                count += 1
+    for i in range(k):
+        if nums[i] < 0:
+            count += 1
+    
+    res.append(count)
+    
+    while right <= len(nums) - 1:
+        left_prev = nums[left - 1]
+        cur_right = nums[right]
+        if left_prev < 0 and cur_right > 0:
+            count -= 1
+        elif left_prev > 0 and cur_right  < 0:
+            count -= 1
+        left += 1
+        right += 1
         res.append(count)
+
     return res
+
+
+
+print(negatives_count_in_every_sublist([2, -1, -3, 4, 1, 1, -4], 3)) #[2, 2, 1, 0, 1]
