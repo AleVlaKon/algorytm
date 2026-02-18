066fbcb5ab7e90ae45b9527dcf0653b2b274f301
AleVlaKon <kontikov@mail.ru>
2026-02-18 10:28:16 +0300

    Решил задачу 10.5.3 (просто забыл про >=)

--- 10/10.5/10.5.3.py
+++ 10/10.5/10.5.3.py
@@ -1,15 +1,26 @@
 def negatives_count_in_every_sublist(nums, k):
     left = 0
-    right = k
+    right = k - 1
     res = []
+    count = 0
 
-    for i in range(right, len(nums) + 1):
-        print(nums[i - k:i])
-        count = 0
-        for j in range(i - k, i):
-            if nums[j] < 0:
-                count += 1
+    for i in range(k):
+        if nums[i] < 0:
+            count += 1
+    
+    res.append(count)
+    
+    while right < len(nums) - 1:
+        cur_left = nums[left]
+        cur_right = nums[right + 1]
+        if cur_left < 0 and cur_right >= 0:
+            count -= 1
+        elif cur_left >= 0 and cur_right  < 0:
+            count += 1
+        left += 1
+        right += 1
         res.append(count)
+
     return res
 
 
--- tests/testing_func.py
+++ tests/testing_func.py
@@ -1,6 +1,6 @@
 def negatives_count_in_every_sublist(nums, k):
-    left = 1
-    right = k
+    left = 0
+    right = k - 1
     res = []
     count = 0
 
@@ -10,13 +10,13 @@ def negatives_count_in_every_sublist(nums, k):
     
     res.append(count)
     
-    while right <= len(nums) - 1:
-        left_prev = nums[left - 1]
-        cur_right = nums[right]
-        if left_prev < 0 and cur_right > 0:
+    while right < len(nums) - 1:
+        cur_left = nums[left]
+        cur_right = nums[right + 1]
+        if cur_left < 0 and cur_right >= 0:
             count -= 1
-        elif left_prev > 0 and cur_right  < 0:
-            count -= 1
+        elif cur_left >= 0 and cur_right  < 0:
+            count += 1
         left += 1
         right += 1
         res.append(count)
@@ -24,5 +24,11 @@ def negatives_count_in_every_sublist(nums, k):
     return res
 
 
-
-print(negatives_count_in_every_sublist([2, -1, -3, 4, 1, 1, -4], 3)) #[2, 2, 1, 0, 1]
+# nums = [-2, 6, 3, -7, -5, -2, 9, -3, -8, 0]
+# print(negatives_count_in_every_sublist(nums, 5)) # [3, 3, 3, 4, 4, 3]
+# print(negatives_count_in_every_sublist([2, -1, -3, 4, 1, 1, -4], 3)) #[2, 2, 1, 0, 1]
+# print(negatives_count_in_every_sublist([1, 2, 3, 4, 5], 2))
+# print(negatives_count_in_every_sublist([-1], 1))
+# print(negatives_count_in_every_sublist([-5, -1, -6, -2, -2], 1))
+# print(negatives_count_in_every_sublist([-1, -1, -1, -1, -1], 5))
+# print(negatives_count_in_every_sublist([4, 0, -1, 2, 3, -5], 3))
