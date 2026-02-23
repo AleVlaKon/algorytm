4982b2d819df80ef997252f4f0858ea27b8d433e
AleVlaKon <kontikov@mail.ru>
2026-02-23 20:33:52 +0300

    Задача 10.4.5 (через множество, не работает)

---
+++ 10/10.4.5.py
@@ -0,0 +1,28 @@
+def has_duplicates_within_range(nums, k):
+    left = 0
+    right = k - 1
+    diapazon = set(nums[:k - 1])
+    print(left, right, diapazon)
+    n = len(nums)
+
+    if len(diapazon) < len(nums[:k - 1]):
+        return True
+
+    while right < n:
+        left_elem = nums[left]
+        right_elem = nums[right]
+        if right_elem in diapazon:
+            return True
+        else:
+            diapazon.add(right_elem)
+            diapazon.remove(left_elem)
+            left += 1
+            right += 1
+    
+    return False
+
+
+# print(has_duplicates_within_range([3, 1, 5, -4, 5, 2], 3))    # дубликаты: 5
+# print(has_duplicates_within_range([1, 1, 5, -4, 2, 5], 3))    # дубликаты: 1, 5
+# print(has_duplicates_within_range([5, 1, 3, -4, 2, 5], 3))    # дубликатов нет
+print(has_duplicates_within_range([-2, -1, -2, -1], 2))       # дубликаты: -2, -1
