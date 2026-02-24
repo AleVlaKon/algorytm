06fda03b7bacc3cbbbdff79346cd482699b4fbc6
AleVlaKon <kontikov@mail.ru>
2026-02-24 10:10:50 +0300

    Решил задачу 10.5.6 (через словарь с помощью deepseek)

--- 10/10.5/10.5.6.py
+++ 10/10.5/10.5.6.py
@@ -1,19 +1,33 @@
+from collections import defaultdict
+
 def uniques_count_in_every_sublist(nums, k):
     left = 0
     right = k
     n = len(nums)
-    res = []
+    diapazon = defaultdict(int) 
 
-    while right <= n:
-        diapazon = set(nums[left:right])
+    for i in range(k):
+        diapazon[nums[i]] += 1
+
+    res = [len(diapazon)]
+
+    while right < n:
+        left_elem = nums[left]
+        diapazon[left_elem] -= 1
+        if diapazon[left_elem] == 0:
+            del diapazon[left_elem]
+
+        right_elem = nums[right]
+        diapazon[right_elem] += 1
         res.append(len(diapazon))
+        
         left += 1
         right += 1
 
     return res
 
 
-print(uniques_count_in_every_sublist([2, 5, 5, 5, -3, 1, -3], 3))
+print(uniques_count_in_every_sublist([2, 5, 5, 5, -3, 1, -3], 3)) # [2, 1, 2, 3, 2]
 print(uniques_count_in_every_sublist([1, 2, 3, 4, 5], 5))
 print(uniques_count_in_every_sublist([1], 1))
 print(uniques_count_in_every_sublist([2, 2, 2, 2, 2], 2))
--- tests/testing_func.py
+++ tests/testing_func.py
@@ -1,12 +1,26 @@
+from collections import defaultdict
+
 def uniques_count_in_every_sublist(nums, k):
     left = 0
     right = k
     n = len(nums)
-    res = []
+    diapazon = defaultdict(int) 
 
-    while right <= n:
-        diapazon = set(nums[left:right])
+    for i in range(k):
+        diapazon[nums[i]] += 1
+
+    res = [len(diapazon)]
+
+    while right < n:
+        left_elem = nums[left]
+        diapazon[left_elem] -= 1
+        if diapazon[left_elem] == 0:
+            del diapazon[left_elem]
+
+        right_elem = nums[right]
+        diapazon[right_elem] += 1
         res.append(len(diapazon))
+        
         left += 1
         right += 1
 
