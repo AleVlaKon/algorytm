70c38db249e04d267554b6390706f0317e863dd6
AleVlaKon <kontikov@mail.ru>
2026-02-26 11:58:21 +0300

    Решаю задачу 8.3.16 (бинарная сортировка вставками)

---
+++ 8/8.3.16.py
@@ -0,0 +1,45 @@
+def binary_search(nums, k):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        middle_idx = (left + right) // 2
+        center_elem = nums[middle_idx]
+
+        if k == center_elem:
+            return middle_idx
+        if k > center_elem:
+            left = middle_idx + 1
+        if k < center_elem:
+            right = middle_idx - 1
+
+    return left
+
+
+# nums = [1, 2, 4, 5]
+# print(binary_search(nums, 3))
+# print(binary_search(nums, 4))
+# print(binary_search(nums, 5))
+# print(binary_search(nums, 6))
+# print(binary_search(nums, 0))
+        
+
+def binary_insertion_sort(nums):
+    n = len(nums)
+
+    for i in range(n):
+        element = nums[i]
+        incert_position = binary_search(nums[:i], element)
+
+        for j in range(incert_position + 1, i - 1, -1):
+            nums[j + 1] = nums[j]
+
+        nums[incert_position] = element
+            
+nums = [3, 4, 1, 2, 5]
+binary_insertion_sort(nums)
+print(nums)
+
+
+        
+        
