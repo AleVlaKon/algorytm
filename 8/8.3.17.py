f836071b2d53a72afdcb74bd593488864ce14a98
AleVlaKon <kontikov@mail.ru>
2026-03-04 20:12:12 +0300

    Сделана задача 8.3.17 (закрыл сортировку вставками)

--- 8/8.3.17.py
+++ 8/8.3.17.py
@@ -1,43 +1,16 @@
-from itertools import combinations
-
-def binary_search(nums, k):
-    left = 0
-    right = len(nums) - 1
-
-    while left <= right:
-        middle_idx = (left + right) // 2
-        center_elem = nums[middle_idx]
-
-        if k == center_elem:
-            return middle_idx
-        if k > center_elem:
-            left = middle_idx + 1
-        if k < center_elem:
-            right = middle_idx - 1
-
-    return left
-
-
-def section_sort(nums: list[int]) -> None:
-    zeros = [-1]
-    zeros_2 = [i for i in range(len(nums)) if nums[i] == 0]
-    zeros.extend(zeros_2)
-    zeros.append(len(nums) - 1)
-    print(zeros)
-
-    for left in range(len(zeros) - 1):
-        left_gr = zeros[left]
-        right_br = zeros[left + 1]
-
-        for i in range(left_gr, right_br):
-            element = nums[i]
-            incert_position = binary_search(nums[left_gr:i], element)
-
-            for j in range(i - 1, incert_position - 1, -1):
-                nums[j + 1] = nums[j]
-
-
-            nums[incert_position] = element
+def subsection_search(nums, start, end):
+    for i in range(start + 1, end):
+        elem = nums[i]
+        j = i - 1
+        # пока j указатель меньше начальной позиции
+        # и передвигаемый элемент меньше текущего
+        while j >= start and elem < nums[j]:
+            # перемещаем элементы вперед
+            nums[j + 1] = nums[j]
+            # а затем уменьшаем счетчик
+            j -= 1
+        # а потом ставим элемент в нужное место
+        nums[j + 1] = elem
 
 
 def get_sections_indices(lst):
@@ -59,7 +32,20 @@ def get_sections_indices(lst):
     return sections
 
 
-nums = [2, 1, 0, 3, 2, 1, 0]
-print(get_sections_indices(nums))
+def section_sort(nums: list[int]) -> None:
+    section_list = get_sections_indices(nums)
+
+    for start, end in section_list:
+        subsection_search(nums, start, end)
+
+
+
+# nums = [2, 1, 0, 3, 2, 1, 0]
+# # print(get_sections_indices(nums))
+# # print(subsection_search(nums, 0, 3))
 # section_sort(nums)
 # print(nums)
+        
+nums = [1, 1, 1, 0, 5, 3, 4, 0, 2, 5, 3, 0, 3, 2, 1, 0]
+section_sort(nums)
+print(nums)
--- tests/testing_func.py
+++ tests/testing_func.py
@@ -1,29 +1,39 @@
-def binary_search(nums, k):
-    left = 0
-    right = len(nums) - 1
-
-    while left <= right:
-        middle_idx = (left + right) // 2
-        center_elem = nums[middle_idx]
-
-        if k == center_elem:
-            return middle_idx
-        if k > center_elem:
-            left = middle_idx + 1
-        if k < center_elem:
-            right = middle_idx - 1
-
-    return left
-
-
-def binary_insertion_sort(nums):
-    n = len(nums)
-
-    for i in range(n):
-        element = nums[i]
-        incert_position = binary_search(nums[:i], element)
-
-        for j in range(i - 1, incert_position - 1, -1):
+def subsection_search(nums, start, end):
+    for i in range(start + 1, end):
+        elem = nums[i]
+        j = i - 1
+        # пока j указатель меньше начальной позиции
+        # и передвигаемый элемент меньше текущего
+        while j >= start and elem < nums[j]:
+            # перемещаем элементы вперед
             nums[j + 1] = nums[j]
+            # а затем уменьшаем счетчик
+            j -= 1
+        # а потом ставим элемент в нужное место
+        nums[j + 1] = elem
 
-        nums[incert_position] = element
+
+def get_sections_indices(lst):
+    sections = []
+    i = 0
+    n = len(lst)
+    while i < n:
+        # пропускаем нули
+        if lst[i] == 0:
+            i += 1
+            continue
+        # нашли начало секции
+        start = i
+        # идём до конца секции (пока не встретим ноль или конец списка)
+        while i < n and lst[i] != 0:
+            i += 1
+        end = i  # последний элемент секции
+        sections.append((start, end))
+    return sections
+
+
+def section_sort(nums: list[int]) -> None:
+    section_list = get_sections_indices(nums)
+
+    for start, end in section_list:
+        subsection_search(nums, start, end)
