8ebad8a78b5fe7ee9f900a2981c911c3ae4459c8
AleVlaKon <kontikov@mail.ru>
2026-03-06 09:42:04 +0300

    Решил 10.4.6 (через sort) - меньше памяти, но медленнее

--- 10/10.4/10.4.6.py
+++ 10/10.4/10.4.6.py
@@ -1,15 +1,13 @@
 def count_rescue_attempts(weights: list[int], limit: int):
-    sorted_weights = sorted(weights)
+    weights.sort()
     left = 0
     right = len(weights) - 1
     count = 0
 
     while left <= right:
-        if sorted_weights[left] + sorted_weights[right] > limit:
-            right -= 1
-        else:
+        if weights[left] + weights[right] <= limit:
             left += 1
-            right -= 1
+        right -= 1
         count += 1
 
     return count
